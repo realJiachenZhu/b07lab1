614730644e0e053c38eb33daecbb9f393603164d
realJiachenZhu <realjiachen.zhu@mail.utoronto.ca>
2022-10-01 18:10:30 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,43 @@
 public class Driver {
     public static void main(String [] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
-        }
+    	double [] coef1 = {1, -2, 3};
+        int [] expo1 = {0, 1, 5};
+        Polynomial p1 = new Polynomial(coef1, expo1);
+
+        double[] coef2 = {1, -3, 5, 7};
+        int[] expo2 = {1, 2, 3, 5};
+        Polynomial p2 = new Polynomial(coef2, expo2);
+
+        System.out.println("TEST ADD FUNCTION");
+        System.out.println();
+        Polynomial tmp_p = p1.add(p2);
+        for (int i = 0; i < tmp_p.myArray.length; i++)
+            System.out.print(tmp_p.myArray[i] + " ");
+        System.out.println();
+        System.out.println("Result for coefficients");
+
+         for (int i = 0; i < tmp_p.exponents.length; i++)
+             System.out.print(tmp_p.exponents[i] + " ");
+         System.out.println();
+         System.out.println("Result for exponents");
+
+         System.out.println("TEST MULTIPLY FUNCTION");
+         System.out.println();
+         tmp_p = p1.multiply(p2);
+         for (int i = 0; i < tmp_p.myArray.length; i++)
+             System.out.print(tmp_p.myArray[i] + " ");
+         System.out.println();
+         System.out.println("Result for coefficients");
+
+         for (int i = 0; i < tmp_p.exponents.length; i++)
+             System.out.print(tmp_p.exponents[i] + " ");
+         System.out.println();
+         System.out.println("Result for exponents");
+         
+         System.out.println("TEST HASROOT FUNCTION");
+         System.out.println();
+         System.out.println(p1.hasRoot(-1));
+         System.out.println(p2.hasRoot(0));
+         System.out.println();
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,32 +1,60 @@
+import java.util.Scanner;
+
 public class Polynomial {
     double[] myArray;
+    int[] exponents;
 
     public Polynomial(){
         this.myArray = new double[1];
-        this.myArray[0] = 0.0; 
+        this.exponents = new int[1]; 
     }
 
-    public Polynomial(double [] anotherArray){
+    public Polynomial(double [] anotherArray, int[] exponents){
         this.myArray = anotherArray;
+        this.exponents = exponents;
     }
+    
+    /*
+    public Polynomial(File file) {
+    	Scanner input = new Scanner(file);
+    	String line = input.nextLine();
+    	input.close();
+    	printf("I dont know how to do this function...");
+    }
+    
+    public void saveToFile(String filename) {
+    	printf("I dont know how to do this function...");
+    }
+    */
 
     public Polynomial add(Polynomial anotherPoly){
-        int len1 = this.myArray.length;
-        int len2 = anotherPoly.myArray.length;
-        double[] newArray;
-        if(len1 > len2) {
-        	newArray = new double[len1];
-        }else {
-        	newArray = new double[len2];
-        }
+        int len1 = this.exponents.length;
+        int len2 = anotherPoly.exponents.length;
         int i;
-        for(i = 0; i < len1; i++){
-            newArray[i] += this.myArray[i];
+        int count = 0;
+        int max_len = Math.max(this.exponents[len1 - 1], anotherPoly.exponents[len2 - 1]);
+        double[] newpolylist = new double[max_len + 1];
+        for(i = 0; i < len1; i++) {
+        	newpolylist[this.exponents[i]] += this.myArray[i];
         }
-        for(i = 0; i < len2; i++){
-            newArray[i] += anotherPoly.myArray[i];
+        for(i = 0; i < len2; i++) {
+        	newpolylist[anotherPoly.exponents[i]] += anotherPoly.myArray[i];
         }
-        return new Polynomial(newArray);
+        for(i = 0; i < max_len + 1; i++) {
+        	if(newpolylist[i] != 0) {
+        		count++;
+        	}
+        }
+        double[] finalpolylist = new double[count];
+        int[] finalexpolist = new int[count];
+        for(i = 0, count = 0; i < max_len + 1; i++) {
+        	if(newpolylist[i] != 0) {
+        		finalpolylist[count] = newpolylist[i];
+        		finalexpolist[count] = i;
+        		count++;
+        	}
+        }
+        return new Polynomial(finalpolylist, finalexpolist);
     }
 
     public double evaluate(double variable){
@@ -34,12 +62,56 @@ public class Polynomial {
         double result = 0;
         int i;
         for(i = 0; i < len; i++){
-            result += Math.pow(variable, i) * this.myArray[i];
+            result += Math.pow(variable, this.exponents[i]) * this.myArray[i];
         }
         return result;
     }
 
     public boolean hasRoot(double target){
         return evaluate(target) == 0.0;
+    }
+    
+    public Polynomial multiply(Polynomial anotherPoly){
+    	int len1 = this.exponents.length;
+        int len2 = anotherPoly.exponents.length;
+        int max_len = this.exponents[len1 - 1] + anotherPoly.exponents[len2 - 1] + 1;
+        int i;
+        int j;
+        int count;
+        double curpoly1;
+        double curpoly2;
+        int curexpo1;
+        int curexpo2;
+        double newpoly;
+        int newexpo;
+        double[] newpolylist = new double[max_len];
+        for(i = 0; i < len1; i++){
+        	curpoly1 = this.myArray[i];
+        	curexpo1 = this.exponents[i];
+        	for(j = 0; j < len2; j++) {
+        		curpoly2 = anotherPoly.myArray[j];
+        		curexpo2 = anotherPoly.exponents[j];
+        		newpoly = curpoly1 * curpoly2;
+        		newexpo = curexpo1 + curexpo2;
+        		newpolylist[newexpo] += newpoly; 
+        	}
+        }
+        j = 0;
+        for(count = 0; count < max_len; count++) {
+        	if(newpolylist[count] != (double) 0) {
+        		j++;
+        	}
+        }
+        double[] finalpolylist = new double[j];
+        int[] finalexpolist = new int[j];
+        count = 0;
+        for(j = 0; j < max_len; j++) {
+        	if(newpolylist[j] != 0) {
+        		finalexpolist[count] = j;
+        		finalpolylist[count] = newpolylist[j];
+        		count++;
+        	}
+        }
+        return new Polynomial(finalpolylist, finalexpolist);
     }
 }
